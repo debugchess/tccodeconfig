488c34be6bb00878833b61c305e14a767698dff5
debugchess <81397269+debugchess@users.noreply.github.com>
2025-03-08 21:34:08 -0500

    Update settings.kts 3

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,16 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.*
-import jetbrains.buildServer.configs.kotlin.buildSteps.script
 
-version = "2024.12"
+version = "2024.03"
 
 project {
-  buildType {
-    id("Tccodeconfig")
-    name = "Hello world"
-    steps {
-        script {
-            scriptContent = "echo 'Hello world!'"
-        }
-    }
-  }
+    description = "Test Project"
 }
